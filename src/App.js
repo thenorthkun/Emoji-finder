9be159a0b9dfe6921fb8d9c7968a24fdfb9105f5
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-08-17 16:29:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,37 +7,48 @@ var emojis = {
   "🤩": "Star-Struck",
   "🙌": "Raising Hands",
   "💪": "Flexed Biceps",
-  "😑": "annoyance" 
+  "😑": "annoyance",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤔": "Thinking Face",
+  "🤗": "Hugging Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨":"Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+ 
 };
+
 var emojiKeys = Object.keys(emojis);
 export default function App() {
-	const [userInputMeaning, emojiMeaning] = useState("");
+  const [userInputMeaning, emojiMeaning] = useState("");
 
-	function emojiInput(i) {
-		var Input = i.target.value;
-		var meaning = emojis[Input];
-		if (meaning === undefined) {
-			meaning = "Emoji not found in Database";
-		}
-		emojiMeaning(meaning);
-	}
+  function emojiInput(i) {
+    var Input = i.target.value;
+    var meaning = emojis[Input];
+    if (meaning === undefined) {
+      meaning = "Emoji not found in Database";
+    }
+    emojiMeaning(meaning);
+  }
 
-	function emojiClickHandler(emoji) {
-		var meaning = emojis[emoji];
-		emojiMeaning(meaning);
-	}
+  function emojiClickHandler(emoji) {
+    var meaning = emojis[emoji];
+    emojiMeaning(meaning);
+  }
 
-	return (
-		<div className="App">
-			<h1>Emoji finder </h1>
-			<input onChange={emojiInput} />
-			<h2>{userInputMeaning}</h2>
-			<h2>What we know</h2>
-			{emojiKeys.map(function (keys) {
-				return (
-					<span onClick={() => emojiClickHandler(keys)}>{keys}</span>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Emoji finder</h1>
+      <input onChange={emojiInput} />
+      <h2>{userInputMeaning}</h2>
+      <h2>What we know</h2>
+      {emojiKeys.map(function (keys) {
+        return <span onClick={() => emojiClickHandler(keys)}>{keys}</span>;
+      })}
+    </div>
+  );
 }
